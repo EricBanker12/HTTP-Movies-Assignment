f9bb7456b9c67fe384dab73811d16ac078ebaa90
ericbanker12 <ericbanker12@gmail.com>
2019-10-17 16:02:07 -0500

    Add styling and labels to MovieForm

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -16,12 +16,12 @@ function MovieForm(props) {
             .then(res => {
                 setInput({
                     ...res.data,
-                    metascore: Number(res.data.metascore),
-                    stars: res.data.stars.join(', ')
+                    metascore: String(res.data.metascore),
+                    stars: res.data.stars.join(', '),
                 })
             })
             .catch(err => console.log(err.response));
-    },[])
+    },[id])
 
     function inputHandler(e) {
         setInput({...input, [e.target.name]: e.target.value})
@@ -44,44 +44,57 @@ function MovieForm(props) {
     }
 
     return (
-        <form onSubmit={submitHandler}>
+        <form className="movie-card" onSubmit={submitHandler}>
             <h4>Edit a Movie</h4>
-            <input
-                type='text'
-                name='title'
-                placeholder='Title'
-                title='Title'
-                value={input.title}
-                onChange={inputHandler}
-            />
-            <input
-                type='text'
-                name='director'
-                placeholder='Director'
-                title='Director'
-                value={input.director}
-                onChange={inputHandler}
-            />
-            <input
-                type='number'
-                name='metascore'
-                placeholder='Metascore'
-                pattern='\d{1,2}|100' // 0-100
-                min='0'
-                max='100'
-                title='Metascore must be a number between 0 and 100'
-                value={input.metascore}
-                onChange={inputHandler}
-            />
-            <input
-                type='text'
-                name='stars'
-                placeholder='Star Actor(s) (for multiple, separate by comma: ",")'
-                title='Star Actor(s) (for multiple, separate by comma: ",")'
-                value={input.stars}
-                onChange={inputHandler}
-            />
-            <button>Submit</button>
+            <label style={{display: 'block', margin: '1rem 0'}}>
+                Title:&nbsp;
+                <input
+                    type='text'
+                    name='title'
+                    placeholder='Title'
+                    title='Title'
+                    value={input.title}
+                    onChange={inputHandler}
+                />
+            </label>
+            <label style={{display: 'block', margin: '1rem 0'}}>
+                Director:&nbsp;
+                <input
+                    type='text'
+                    name='director'
+                    placeholder='Director'
+                    title='Director'
+                    value={input.director}
+                    onChange={inputHandler}
+                />
+            </label>
+            <label style={{display: 'block', margin: '1rem 0'}}>
+                Metascore:&nbsp;
+                <input
+                    type='number'
+                    name='metascore'
+                    placeholder='Metascore'
+                    pattern='\d{1,2}|100' // 0-100
+                    min='0'
+                    max='100'
+                    title='Metascore must be a number between 0 and 100'
+                    value={input.metascore}
+                    onChange={inputHandler}
+                />
+            </label>
+            <label style={{display: 'block', margin: '1rem 0'}}>
+                Star Actor(s):&nbsp;
+                <input
+                    type='text'
+                    style={{width: '75%'}}
+                    name='stars'
+                    placeholder='Star Actor(s) (for multiple, separate by comma: ",")'
+                    title='Star Actor(s) (for multiple, separate by comma: ",")'
+                    value={input.stars}
+                    onChange={inputHandler}
+                />
+            </label>
+            <button className='home-button'>Submit</button>
         </form>
     )
 }
